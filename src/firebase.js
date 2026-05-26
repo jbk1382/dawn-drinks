e9daa2b901990a2e3f81d4393ee156d0e84b72ed
새아이디 <@gmail.com>
2026-05-26 23:30:25 +0900

    Firebase 설정 업데이트

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA7SbE8MUMarsXvEPjTx6yZb5GHtRd6Q1w",
-  authDomain: "daun-drinks-8ce01.firebaseapp.com",
-  projectId: "daun-drinks-8ce01",
-  storageBucket: "daun-drinks-8ce01.firebasestorage.app",
-  messagingSenderId: "884975663637",
-  appId: "1:884975663637:web:d7d6f8ea959f0515a56c1c"
+  apiKey: "AIzaSyCEKr4-mG3_GsDR-hXRSM62j8dwGazmD6U",
+  authDomain: "daun-drinks-cd57f.firebaseapp.com",
+  projectId: "daun-drinks-cd57f",
+  storageBucket: "daun-drinks-cd57f.firebasestorage.app",
+  messagingSenderId: "654475550073",
+  appId: "1:654475550073:web:b04955e6eeeb2b5abb23a8"
 };
 
 const app = initializeApp(firebaseConfig);
